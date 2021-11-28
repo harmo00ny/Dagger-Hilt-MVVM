56cddcd1e3c8253b51787c06c9778ee88f34349f
harmo00ny <harm000ny1225@gmail.com>
2021-11-28 13:17:42 +0900

    Rename package

---
+++ app/src/main/java/com/marysugar/dagger_hilt_mvvm/util/EntityMapper.kt
@@ -0,0 +1,8 @@
+package com.marysugar.dagger_hilt_mvvm.util
+
+interface EntityMapper<Entity, DomainModel> {
+
+    fun mapFromEntity(entity: Entity): DomainModel
+
+    fun mapToEntity(domainModel: DomainModel): Entity
+}
--- app/src/main/java/com/thecode/dagger_hilt_mvvm/MyApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.thecode.dagger_hilt_mvvm
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MyApplication : Application()
